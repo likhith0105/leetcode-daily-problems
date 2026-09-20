58a711232e0c10d0c112b26fa74de1b42eef1350
LIKHITH  HP <hplikhith690@gmail.com>
2026-09-20 15:07:01 +0530

    Add files via upload

---
+++ Reverse Degree of a String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        ans = 0
+        
+        for i, ch in enumerate(s, 1):
+            value = 26 - (ord(ch) - ord('a'))
+            ans += value * i
+        
+        return ans
